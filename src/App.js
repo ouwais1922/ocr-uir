9bb999c102e4992c5d43830098481b5b70cb0322
Zlaigi <ouwaisszlaigi2@gmail.com>
2023-09-10 17:30:14 +0100

    mm'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Document from "./components/Document";
+import Nopage from "./components/Nopage";
 
 function App() {
   return (
-    <>
-    <Dashboard></Dashboard>
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/documents" element={<Document />}></Route>
+      <Route path="*" element={<Nopage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -1,11 +1,162 @@
-import React from 'react'
-
+import React from "react";
+import SideBar from "./SideBar";
 const Document = () => {
   return (
     <>
-    
+      <SideBar></SideBar>
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200  rounded-lg dark:border-gray-700">
+          <div className="grid grid-cols-1 mb-4">
+            <div className="flex items-center justify-start h-24 rounded ">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                New Template
+              </p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 mb-4">
+            <div className="flex items-center justify-start rounded ">
+              <div className="w-full">
+                <label
+                  for="last_name"
+                  class="block mb-2 text-sm font-medium text-gray-400 dark:text-gray-500"
+                >
+                  Titre
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 text-gray-400 dark:text-gray-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Titre"
+                  required
+                />
+              </div>
+              <div></div>
+            </div>
+            <div className="flex items-center justify-start rounded ">
+              <div className="w-full">
+                <label
+                  for="last_name"
+                  class="block mb-2 text-sm font-medium text-gray-400 dark:text-gray-500"
+                >
+                  Description
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:placeholder-gray-400 text-gray-400 dark:text-gray-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Description"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div class="grid grid-cols-2 gap-4 mb-10 ">
+            <div class="flex items-center justify-start rounded h-28 ">
+              <button
+                type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Upload
+              </button>
+            </div>
+            <div class="flex items-center justify-center rounded bg-gray-50 h-96 ">
+              <p class="text-2xl text-gray-400 dark:text-gray-500">Image</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-end h-12 mb-4 rounded">
+            <button
+              type="button"
+              class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            >
+              Add zone
+            </button>
+          </div>
+          <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 ">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
+                <tr>
+                  <th scope="col" class="px-6 py-3">
+                    Product name
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Color
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Category
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Price
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-200">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Apple MacBook Pro 17"
+                  </th>
+                  <td class="px-6 py-4">Silver</td>
+                  <td class="px-6 py-4">Laptop</td>
+                  <td class="px-6 py-4">$2999</td>
+                  <td class="px-6 py-4">
+                    <a
+                      href="#"
+                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Microsoft Surface Pro
+                  </th>
+                  <td class="px-6 py-4">White</td>
+                  <td class="px-6 py-4">Laptop PC</td>
+                  <td class="px-6 py-4">$1999</td>
+                  <td class="px-6 py-4">
+                    <a
+                      href="#"
+                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+                <tr class="bg-white dark:bg-gray-800">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Magic Mouse 2
+                  </th>
+                  <td class="px-6 py-4">Black</td>
+                  <td class="px-6 py-4">Accessories</td>
+                  <td class="px-6 py-4">$99</td>
+                  <td class="px-6 py-4">
+                    <a
+                      href="#"
+                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Document
+export default Document;
